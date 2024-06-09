19db432ab26d1f96823ff04895074c4400d1d5e5
NagIbrahim <hamed_naglaa2021@hotmail.com>
2024-06-09 23:18:56 +0200

    adjust2 media query of the project page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,6 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa6";
 import { IoLogoJavascript } from "react-icons/io5";
 import { BiLogoTypescript } from "react-icons/bi";
-
-/*import CardImg from "../images/imgs1.jpg"*/
 import "./card.css"
 
 function Card({description1,description2,imageSrc,layout}) {
@@ -25,7 +23,7 @@ function Card({description1,description2,imageSrc,layout}) {
        <div id="p2">
           <p>{description2}</p>
        </div>  
-       <div id="b">
+      {/* <div id="b">
         <div id="t"></div>
        <div id="tools">
        <span><FaHtml5 className='icon' /></span>
@@ -34,15 +32,25 @@ function Card({description1,description2,imageSrc,layout}) {
        <span><BiLogoTypescript  className='icon' /></span>
        <span> <FaReact className='icon' /></span>
 
-       </div>   
+       </div> */}
+
 
        </div>
-      
+       
+       
+       <div id="tools">
+       <span><FaHtml5 className='icon' /></span>
+       <span>  <FaCss3Alt className='icon' /> </span>   
+       <span><IoLogoJavascript className='icon' /> </span>
+       <span><BiLogoTypescript  className='icon' /></span>
+       <span> <FaReact className='icon' /></span>
+
+       </div>   
         
         </div>
       
       </div>
-    </div>
+    
   )
 }
 
