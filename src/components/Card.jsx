e15164dc7f3196cbe9b8890df66c6859137a23d2
NagIbrahim <hamed_naglaa2021@hotmail.com>
2024-06-10 11:49:35 +0200

    add title to the project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,8 +11,10 @@ function Card({description1,description2,imageSrc,layout}) {
     <div   id="card"  className={` ${layout}`}>
         
       <div id="card-img" >
+      <div  id="project-title"><p>Project Title</p></div>
        {/*  <img src={imageSrc} alt="img" /> */ }
        <img src={imageSrc} alt="img" />
+      
       </div>
       <div className="c-txt">
         <div className="txt-w">
@@ -23,20 +25,8 @@ function Card({description1,description2,imageSrc,layout}) {
        <div id="p2">
           <p>{description2}</p>
        </div>  
-      {/* <div id="b">
-        <div id="t"></div>
-       <div id="tools">
-       <span><FaHtml5 className='icon' /></span>
-       <span>  <FaCss3Alt className='icon' /> </span>   
-       <span><IoLogoJavascript className='icon' /> </span>
-       <span><BiLogoTypescript  className='icon' /></span>
-       <span> <FaReact className='icon' /></span>
 
-       </div> */}
-
-
-       </div>
-       
+       </div>     
        
        <div id="tools">
        <span><FaHtml5 className='icon' /></span>
